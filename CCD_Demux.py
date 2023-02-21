84b4278ac43c3e904eb63ffbc86338cce8a593d9
oscar <oscarmanuelbaez.123@gmail.com>
2023-02-21 14:06:28 -0300

    Update CCD_Demux.py

--- CCD_Demux.py
+++ CCD_Demux.py
@@ -89,44 +89,34 @@ if __name__ == '__main__':
     LTA_channel=4
     CCDinMCM=16 
     gains=[]
-    #img_CCD16=fits.HDUList([])
-    #print(hdulist[4].header)
 
     Map=[1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16]
-    #Map=[1,5,12,16,2,9,13,6,3,10,14,7,4,11,15,8]
     for N in range(int(NSAMP/16)):  #Se recorre nsamp/16=7 veces por cada imagen    
-    #for N in range(1):
-        #Primaryhdu_MCM = fits.PrimaryHDU()
         Primaryhdu_MCM = fits.PrimaryHDU(header=hdulist[0].header)
         img_CCD16=fits.HDUList([Primaryhdu_MCM]) #Se crearan nro_imagenes*nsamp/16 (Ej: 5*112/6=35)
-        #img_CCD16[0].header=hdulist[0].header
-        #print(img_CCD16[0].header)
         for CCD in Map:                               #Se recorre 16 veces para ir agarrando 16 CCD 
             img_parcial=GetSingleCCDImage(hdulist,LTA_channel,CCD-1+CCDinMCM*N,NCOL,NAXIS2,CCDNCOL,NSAMP)
-            print(img_parcial.dtype)
-            print (CCD)
             img_name="CCD "+str(CCD)+" in MCM "+str(N+1)
             image_hdu=fits.ImageHDU(data=img_parcial,header=hdulist[LTA_channel].header,name=img_name)
-            #image_hdu.header=hdulist[LTA_channel].header
             image_hdu.verify('silentfix') 
+
             ############################################################
             if args[1]==str("yes"):
                 gain=CalcGain(img_parcial,NAXIS2) #Lineas encargada del histograma.
                 print("CCD ",(N*16+CCD)+1," ",gain)
                 gains.append(gain)
             ############################################################
-            #img_CCD16.append(fits.ImageHDU(img_parcial))
             img_CCD16.append(image_hdu)
+
         #Proceso de guardado
         Directory_Demux="Demuxed_"+baseName+"/"
         if not os.path.exists(Directory_Demux):
             os.makedirs(Directory_Demux)
         SaveName=str(Directory_Demux+"PROC_MCM"+str(N+1)+"_Demuxed_"+baseName+".fits") 
-        #img_CCD16.writeto(SaveName,overwrite=True)
-        img_CCD16.verify('fix') 
         img_CCD16.writeto(SaveName,overwrite=True)
         img_CCD16.clear()
         print((N+1)/(NSAMP/16)*100,"% done...")
+        
     hdulist.close()
     ############################################################
     if args[1]==str("yes"):
